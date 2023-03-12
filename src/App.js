9d1500cfdf2f407d9ebde545a9057e9c99662d89
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-12 20:59:47 +0300

    month sales analysis

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Business Request</h1>
+          <h2>
+            Create visual to display the correlation between the Actual and
+            Budgets Monthly phone sales
+          </h2>
         </p>
+        
+        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docs.google.com/spreadsheets/d/1ETQQwPaX8kMLgqIBLBulHTQiVk_nMf0rXOLxm4UnLko/edit#gid=0"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          View the Dashboard Here
         </a>
+       
       </header>
     </div>
   );
